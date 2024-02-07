7901580cafcbd0f9cb3c9c6c7ab6659b2ce02dbd
Pavel Rysnik <pavelrysnik@gmail.com>
2024-02-07 23:08:04 +0300

    feat: migrate to multimodule with custom cache starter

---
+++ clevertec-house-person/src/main/java/ru/clevertec/house/dto/HouseResponse.java
@@ -0,0 +1,45 @@
+package ru.clevertec.house.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.github.sakujj.cache.IdentifiableByUUID;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+import ru.clevertec.house.constant.FormatConstants;
+import ru.clevertec.house.documentation.OpenApiSchema;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+public class HouseResponse implements IdentifiableByUUID {
+    @Schema(example = OpenApiSchema.Examples.HouseDTO.UUID_EXAMPLE)
+    private UUID uuid;
+
+    @Schema(exclusiveMinimum = true,
+            minimum = OpenApiSchema.Minimums.HouseDTO.AREA_EXCLUSIVE_MINIMUM,
+            example = OpenApiSchema.Examples.HouseDTO.AREA_EXAMPLE)
+    private Double area;
+
+    @Schema(description = OpenApiSchema.Descriptions.NOT_BLANK_DESCRIPTION,
+            example = OpenApiSchema.Examples.HouseDTO.COUNTRY_EXAMPLE)
+    private String country;
+
+    @Schema(description = OpenApiSchema.Descriptions.NOT_BLANK_DESCRIPTION,
+            example = OpenApiSchema.Examples.HouseDTO.CITY_EXAMPLE)
+    private String city;
+
+    @Schema(description = OpenApiSchema.Descriptions.NOT_BLANK_DESCRIPTION,
+            example = OpenApiSchema.Examples.HouseDTO.STREET_EXAMPLE)
+    private String street;
+
+    @Schema(exclusiveMinimum = true,
+            minimum = OpenApiSchema.Minimums.HouseDTO.NUMBER_EXCLUSIVE_MINIMUM,
+            example = OpenApiSchema.Examples.HouseDTO.NUMBER_EXAMPLE)
+    private Integer number;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = FormatConstants.DATE_TIME_FORMAT)
+    @Schema(pattern = OpenApiSchema.Patterns.DATE_TIME_FORMAT)
+    private LocalDateTime createDate;
+}
--- src/test/java/ru/clevertec/house/cache/house/HouseLRUCacheTestsHouse.java
+++
@@ -1,7 +0,0 @@
-package ru.clevertec.house.cache.house;
-
-import org.springframework.test.context.ActiveProfiles;
-
-@ActiveProfiles({ "enable-cache", "cache-lru"})
-public class HouseLRUCacheTestsHouse extends HouseAbstractCacheTests {
-}
